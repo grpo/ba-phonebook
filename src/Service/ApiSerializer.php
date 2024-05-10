e1fcc7ae75e20045dbd465206e3a0bd9a0f256b6
Povilas Grelis <grepov@gmail.com>
2024-05-10 20:30:00 +0300

    fixed return value; add handle for empty request body

--- src/Service/ApiSerializer.php
+++ src/Service/ApiSerializer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Constants\AbstractSerializationConstants;
+use App\Exception\ApiException;
 use JMS\Serializer\DeserializationContext;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
@@ -30,11 +31,24 @@ class ApiSerializer
         string $class,
         array $contextGroups = [AbstractSerializationConstants::GROUP_DEFAULT]
     ): object {
+        $this->checkIsValidJson($json);
         return $this->serializer->deserialize(
             $json,
             $class,
             'json',
             DeserializationContext::create()->setGroups($contextGroups)
         );
+    }
+
+    /**
+     * @throws ApiException
+     */
+    private function checkIsValidJson($data): bool
+    {
+        if (is_string($data) && json_validate($data)) {
+            return true;
+        }
+
+        throw new ApiException('Invalid JSON data.');
     }
 }
